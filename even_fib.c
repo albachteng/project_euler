aa31e500d7f6dfa117d1d2b2a0adfa951cda59c0
Graham Albachten <albachteng@gmail.com>
2023-04-08 10:48:51 -0500

    add max and sum

--- even_fib.c
+++ even_fib.c
@@ -1,10 +1,12 @@
+/* project euler #2, sum of even fibonacci numbers not exceeding 4 million */
+
 #include <stdio.h>
 
 #define MAX 100
 
-int cache[MAX];
+unsigned int cache[MAX];
 
-int memoized_fib(int index);
+unsigned int memoized_fib(int index);
 
 void init_cache() {
   for (int i = 0; i < MAX; i++) {
@@ -16,15 +18,28 @@ void init_cache() {
 
 int main() {
   init_cache();
-  printf("%d", memoized_fib(20));
+  int fib, sum, i, j = 0;
+  for (i = 1; i < MAX; i++) {
+    if (memoized_fib(i) > 4000000) {
+      printf("%d: %u\n", i, memoized_fib(i));
+      break;
+    }
+  }
+
+  for (j = 1; j < i; j++) {
+    if (memoized_fib(j) % 2 == 0) {
+      sum += memoized_fib(j);
+    }
+  }
+  printf("%d", sum);
   return 1;
 }
 
-int memoized_fib(int index) {
+unsigned int memoized_fib(int index) {
   if (cache[index] != 0) {
     return cache[index];
   } else {
-    int first, second;
+    unsigned int first, second;
     first = memoized_fib(index - 1);
     second = memoized_fib(index - 2);
     cache[index - 1] = first;
